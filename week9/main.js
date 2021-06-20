bdbcda9c3ec54f3aa1a5b2f135f66cfb7012fd36
Valerie Ludden <valudden@gmail.com>
2021-06-19 23:38:10 -0600

    Week 9 stuff

---
+++ week9/main.js
@@ -0,0 +1,19 @@
+const squareElement = document.getElementById('square');
+let angle = 0;
+setInterval( () => {
+    angle = (angle + 2) % 360;
+    squareElement.style.transform = `rotate(${angle}deg)`
+}, 1000/60);
+
+const squareElement2 = document.getElementById('square2');
+let angle2 = 0;
+function rotate() {
+    angle2 = (angle2 + 2)%360;
+    squareElement2.style.transform = `rotate(${angle2}deg)`
+    window.requestAnimationFrame(rotate);
+}
+const id = requestAnimationFrame(rotate);
+
+function stop(){
+    cancelAnimationFrame(id);
+}
